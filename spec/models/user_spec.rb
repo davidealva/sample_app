e85a06293bb63084f124f590d89c38f5ca17477f
David Alvarado <davidealva@gmail.com>
2012-12-26 13:41:18 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -25,7 +27,7 @@ describe User do
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
-  end
+  end 
 
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
@@ -96,5 +98,9 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
